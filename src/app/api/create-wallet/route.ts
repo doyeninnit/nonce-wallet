b8c20787d9b2e1094d816ad710c2bc6a624a052b
Nashons <agatenashons@gmail.com>
2023-11-30 16:08:56 -0500

    signers module v1

--- src/app/api/create-wallet/route.ts
+++ src/app/api/create-wallet/route.ts
@@ -2,10 +2,45 @@ import { prisma } from "@/utils/db";
 import { walletFactoryContract } from "@/utils/getContracts";
 import { randomBytes } from "crypto";
 import { NextRequest, NextResponse } from "next/server";
+import { ethers } from 'ethers';
+import bcrypt from 'bcrypt'; // For password hashing
 
 export async function POST(req: NextRequest) {
   try {
-    const { signers }: { signers: string[] } = await req.json();
+    // const { signers }: { signers: string[] } = await req.json();
+    const { username, email, password }: { username: string; email: string; password: string } = await req.json();
+    // Check if username or email already exists
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        OR: [
+          { username: username },
+          { email: email }
+        ]
+      }
+    });
+
+    if (existingUser) {
+      return NextResponse.json({ error: "Username or email already exists." });
+    }
+
+    // Generate wallet
+    const wallet = ethers.Wallet.createRandom();
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10); // Adjust the salt rounds as needed
+
+    // Create user in the database
+    const userEoa = await prisma.user.create({
+      data: {
+        username: username,
+        email: email,
+        password: hashedPassword,
+        privateKey: wallet.privateKey,
+        wallet: wallet.address     
+      }
+    });
+    console.log(userEoa)
+    const signers = [userEoa.wallet]; // Wrap in an array
     const salt = "0x" + randomBytes(32).toString("hex");
 
     const walletAddress = await walletFactoryContract.getAddress(signers, salt);
@@ -18,7 +53,7 @@ export async function POST(req: NextRequest) {
         address: walletAddress,
       },
     });
-
+ 
     return NextResponse.json(response);
   } catch (error) {
     console.error(error);
@@ -26,32 +61,3 @@ export async function POST(req: NextRequest) {
   }
 }
 
-
-// import { prisma } from "@/utils/db";
-// import { walletFactoryContract } from "@/utils/getContracts";
-// import { randomBytes } from "crypto";
-// import { NextRequest, NextResponse } from "next/server";
-
-// export async function POST(req: NextRequest) {
-//   try {
-//     const { signer }: { signer: string } = await req.json(); // Accept a single signer
-//     const salt = "0x" + randomBytes(32).toString("hex");
-
-//     // Pass the single signer in an array to the getAddress function
-//     const walletAddress = await walletFactoryContract.getAddress([signer], salt);
-
-//     const response = await prisma.wallet.create({
-//       data: {
-//         salt: salt,
-//         signers: [signer.toLowerCase()], // Store the single signer in an array
-//         isDeployed: false,
-//         address: walletAddress,
-//       },
-//     });
-
-//     return NextResponse.json(response);
-//   } catch (error) {
-//     console.error(error);
-//     return NextResponse.json({ error });
-//   }
-// }
